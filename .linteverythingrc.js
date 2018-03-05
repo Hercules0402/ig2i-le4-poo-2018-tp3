095de5bd71cdfa6a4498cb8f430a172c78778536
Sébastien CORNUEL <sbastien.cornuel@gmail.com>
2018-03-05 08:43:42 +0100

    Create .linteverythingrc.js

---
+++ .linteverythingrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	ignore: [
+		'node_modules',
+		'package-lock.json',
+		'.git',
+		'.travis.yml',
+		'build',
+		'ig2i-le4-poo-2018-tp3.pdf',
+		'nbproject/project.xml',
+		'hopital/nbproject/build-impl.xml',
+		'hopital/build.xml'
+	]
+};
