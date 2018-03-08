d0f415888603c353f307a93f1ae2b7d712081d23
Sébastien CORNUEL <sbastien.cornuel@gmail.com>
2018-03-08 18:33:18 +0100

    Création du projet

---
+++ src/biere2i/Biere2I.java
@@ -0,0 +1,16 @@
+package biere2i;
+
+/**
+ *
+ * @author user
+ */
+public class Biere2I {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
