6bf48c3a2cbb4dca495949c4fce138cea302d198
Sébastien CORNUEL <sbastien.cornuel@gmail.com>
2018-03-10 13:20:12 +0100

    Question 21

---
+++ src/dao/JpaPlanningDao.java
@@ -0,0 +1,55 @@
+package dao;
+
+import java.util.Collection;
+import metier.Planning;
+
+/**
+ * Représente un DAO de type Planning utilisant comme source de données une bdd.
+ * @author user
+ */
+public class JpaPlanningDao extends JpaDao<Planning> implements PlanningDao {
+	
+	/**
+	 * Constructeur par données.
+	 * @param entite TODO
+	 */
+	public JpaPlanningDao(Class<Planning> entite) {
+		super(entite);
+	}
+
+	@Override
+	public boolean deleteAll() {
+		return super.deleteAll();
+	}
+
+	@Override
+	public Collection<Planning> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Planning find(Integer id) {
+		return super.find(id);
+	}
+
+	@Override
+	public void close() {
+		super.close();
+	}
+
+	@Override
+	public boolean delete(Planning obj) {
+		return super.delete(obj);
+	}
+
+	@Override
+	public boolean update(Planning obj) {
+		return super.update(obj);
+	}
+
+	@Override
+	public boolean create(Planning obj) {
+		return super.create(obj);
+	}
+	
+}
